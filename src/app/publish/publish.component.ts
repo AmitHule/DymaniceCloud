7f92bdb32a54e13686f8cf84d0d3f5dd29dbec30
vishesh1996 <vishesh.dab@volkswagen.co.in>
2019-10-25 11:53:37 +0530

    edited

--- src/app/publish/publish.component.ts
+++ src/app/publish/publish.component.ts
@@ -34,7 +34,7 @@ export class PublishComponent implements OnInit {
       definition_upload: ['', [Validators.required]],
       backend_url: ['', [Validators.required]],
       product_name: ['', [Validators.required]],
-      product_desp: ['', [Validators.required]],
+      product_desp: [''],
       no_of_requests: ['', [Validators.required]],
       no_of_minutes: ['', [Validators.required]]
 
@@ -45,7 +45,6 @@ export class PublishComponent implements OnInit {
 
     this.data.setbasicname(this.registerForm.value.basicname);
     this.data.setpublish_option(this.registerForm.value.publish_option);
-
     this.data.setDisplayname(this.registerForm.value.Displayname);
     this.data.setBasicdescription(this.registerForm.value.Basicdescription);
     this.data.setbussiness_domain(this.registerForm.value.bussiness_domain);
@@ -58,9 +57,15 @@ export class PublishComponent implements OnInit {
     this.data.setproduct_desp(this.registerForm.value.product_desp);
     this.data.setno_of_requests(this.registerForm.value.no_of_requests);
     this.data.setno_of_minutes(this.registerForm.value.no_of_minutes);
+
     //console.log(this.registerForm.value.product_desp);
     this.submitted=true;
-    this.router.navigate(['success']);
+
+    if (this.registerForm.valid) {
+      this.router.navigate(['success'])
+
+
+    }
   }
 
 
