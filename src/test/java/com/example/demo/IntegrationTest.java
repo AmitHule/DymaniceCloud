8cffc5138156b0bdebcfebd578bb847a00060979
Vashist <F8RI623@apac.vwg>
2019-11-06 15:47:37 +0530

    ldap authentication tdd approach final

--- src/test/java/com/example/demo/IntegrationTest.java
+++ src/test/java/com/example/demo/IntegrationTest.java
@@ -15,7 +15,6 @@ import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest(webEnvironment = WebEnvironment.RANDOM_PORT)
-@AutoConfigureMockMvc
 public class IntegrationTest {
     @Value("${local.server.port}")
     private int port;
