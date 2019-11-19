eca05ae457b5c8bbdef2fd7fddbb39a4b3bb7af4
Vashist <F8RI623@apac.vwg>
2019-11-19 14:12:31 +0530

    done

--- src/test/java/com/example/demo/IntegrationTests.java
+++ src/test/java/com/example/demo/IntegrationTests.java
@@ -15,7 +15,7 @@ import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest(webEnvironment = WebEnvironment.RANDOM_PORT)
-public class IntegrationTest {
+public class IntegrationTests {
     @Value("${local.server.port}")
     private int port;
     @Autowired
